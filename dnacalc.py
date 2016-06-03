23954f1482e52633d41b3d9bcc1df7c3fcaf7c2d
Thomas Chartier <chartier@embl.de>
2016-06-03 11:59:49 +0200

    with N-content

--- dnacalc.py
+++ dnacalc.py
@@ -6,6 +6,21 @@ DNASeq = 'ATGAAC'
 print( 'Sequence : ' + DNASeq )
 print('Cagri is a world champion of programming')
 
-SeqLength = len( DNASeq )
+SeqLength = float( len( DNASeq ) )
 
-print( SeqLength )
+print( 'Length is ' + str( SeqLength ) )
+
+NumberA = DNASeq.count('A')
+NumberC = DNASeq.count('C')
+NumberG = DNASeq.count('G')
+NumberT = DNASeq.count('T')
+
+print('A content : ' + str( NumberA / SeqLength ) )
+print('C content : ' + str( NumberC / SeqLength ) )
+print('G content : ' + str( NumberG / SeqLength ) )
+print('T content : ' + str( NumberT / SeqLength ) )
+
+
+
+
+
