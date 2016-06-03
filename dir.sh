88178cd3f2ae280424fd517da1d85afc9ef74ec8
Thomas Chartier <chartier@embl.de>
2016-06-03 11:16:56 +0200

    added dnacalc.py

--- dir.sh
+++ dir.sh
@@ -3,3 +3,9 @@
 ls -la
 pwd
 echo "I'm hungry"
+
+a = 8
+if A > 5:
+	print "pos"
+else :
+	print "neg"
---
+++ dnacalc.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+#
+
+DNASeq = 'ATGAAC'
+
+print ( 'Sequence : ' + DNASeq )
+print ('Cagri is a world champion of programming')
